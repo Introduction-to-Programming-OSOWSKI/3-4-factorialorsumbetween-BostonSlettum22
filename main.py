47092ace1eb70d2e60385be1a9ca2f1c5afa5c0f
BostonSlettum22 <816661@mystma.org>
2021-01-22 16:39:50 +0000

    factOrSum 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#WRITE YOUR CODE IN THIS FILE
+def factOrSum(x,y):
+
+    bazinga = 0
+
+    if y == "sum":
+        for i in range(0, x+1): 
+            bazinga = bazinga + i
+    
+    else:
+        bazinga = 1
+
+        for i in range(1, x+1):
+            bazinga = bazinga * i
+    
+    return bazinga
+
+print(factOrSum(5,"factorial"))
+
